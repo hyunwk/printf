2b0ff3200f57d308ef2895e5d96da11054866a0a
ab8454 <ab8454@naver.com>
2021-07-05 14:14:34 +0900

    feat: working on %d, test %08.5i, 34

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hyunwkim <hyunwkim@42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 13:53:14 by hyunwkim          #+#    #+#             */
-/*   Updated: 2021/07/05 13:12:50 by hyunwkim         ###   ########.fr       */
+/*   Updated: 2021/07/05 14:13:47 by hyunwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -232,9 +232,7 @@ void	print_multi_str(int time, t_info *info)
 	char	c;
 
 	idx = 0;
-	if (info->left_align && info->zero)
-		c = '0';
-	else if (!info->left_align && info->zero)
+	if (info->zero)
 		c = '0';
 	else
 		c = ' ';
@@ -248,13 +246,19 @@ int	print_num(int n, t_info *info)
 	{
 		if (info->prec > get_int_digits(n))
 		{
+			if (is_minus(n))
+			{
+				ft_putchar('-', info);
+				n *= -1;
+				info->num_neg = 1;
+			}
 			info->zero = 1;
 			print_multi_str(info->prec - get_int_digits(n), info);
 			info->zero = 0;
 		}
 		ft_putnbr(n, info);
 		if (info->width > info->prec && info->prec > get_int_digits(n))
-			print_multi_str(info->width - info->prec, info);
+			print_multi_str(info->width - info->prec - info->num_neg, info);
 		else if (info->width > get_int_digits(n))
 			print_multi_str(info->width - get_int_digits(n), info);
 	}
@@ -280,9 +284,61 @@ int	print_num(int n, t_info *info)
 	return (1);
 }
 
+int	print_char(char c, t_info *info)
+{
+	if (info->left_align)
+	{
+		ft_putchar(c, info);
+		if (info->width)
+		{
+			info->zero = 0;
+			print_multi_str(info->width - 1, info);
+		}
+	}
+	else
+	{
+		if (info->width)
+			print_multi_str(info->width - 1, info);
+		ft_putchar(c, info);
+	}
+	return (1);
+}
+
+char	*get_base(char c)
+{
+	if (c == 'p' || c == 'x')
+		return ("0123456789abcdef");
+	else if (c == 'X')
+		return ("0123456789ABCDEF");
+	return (0);
+}
+
+int	print_hex(unsigned long long n, t_info *info)
+{
+	char	*hex_arr;
+	int		i;
+
+	i = 8;
+	hex_arr = (char *)malloc(sizeof(char) * 9);
+	if (!hex_arr)
+		return (ERR);
+	if (info->type == 'p' || info->type == 'x')
+		ft_putstr("0x", 2, info);
+	else if (info->type == 'X')
+		ft_putstr("0X", 2, info);
+	while (n)
+	{
+		hex_arr[i--] = get_base(info->type)[n % 16];
+		n /= 16;
+	}
+	print_str(hex_arr, info);
+	free(hex_arr);
+	return (1);
+}
+
 //#include<stdio.h>
 //int main()
 //{
-//	ft_printf("%-8.5i\n",75);
-//	printf("%-8.5i\n",75);
+//	ft_printf("%-10.5i\n", -216);
+//	printf("%-10.5i\n", -216);
 //}
