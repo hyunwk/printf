10579afac0f0a1f6932f96d00c8d1c1322c08ff8
Hyunwoo Kim <hyunwkim@c10r6s6.42seoul.kr>
2021-07-08 20:50:54 +0900

    feat: done

--- main.c
+++ main.c
@@ -46,44 +46,79 @@ int main()
 	ft_printf("check s\n====\n");
 	ft_printf("%s","");
 	printf("%s\n","");
+	
 	ft_printf("%s"," ");
 	printf("%s\n"," ");
+	
 	ft_printf("%s","a");
 	printf("%s\n","a");
+	
 	ft_printf("%s","asddddddddddddddddf");
 	printf("%s\n","asddddddddddddddddf");
 	
-
 	ft_printf("====\ncheck p\n====\n");
+	
 	ft_printf("%p\n","asddddddddddddddddf");
 	printf("%p\n\n","asddddddddddddddddf");
+	
 	ft_printf("%p\n","");
 	printf("%p\n\n","");
+	
 	char *s = "abc";
 	ft_printf("%p\n",s);
 	printf("%p\n\n",s);
+	
 	ft_printf("%p\n",&s);
 	printf("%p\n\n",&s);
 	
 	ft_printf("====\ncheck d\n====\n");
 	ft_printf("%d\n",123);
 	printf("%d\n",123);
+	
 	ft_printf("%d\n",2147483647);
 	printf("%d\n",2147483647);
+	
 	ft_printf("%d\n",-2147483648);
 	//printf("%d\n",-2147483648);
 	
 	ft_printf("====\ncheck u\n====\n");
 	ft_printf("%u\n",2147483647);
 	printf("%u\n",2147483647);
+	
 	ft_printf("%u\n",4294967295);
 	//printf("%u\n",4294967295);
 	
 	ft_printf("====\ncheck x\n====\n");
 	ft_printf("%x\n",2147483647);
 	printf("%x\n",2147483647);
+	
 	ft_printf("%x\n", 16);
 	printf("%x\n", 16);
+	
 	ft_printf("%x\n", -16);
 	printf("%x\n", -16);
+	
+	ft_printf("====\ncheck X\n====\n");
+	ft_printf("%X\n",2147483647);
+	printf("%X\n",2147483647);
+	
+	ft_printf("%X\n", 16);
+	printf("%X\n", 16);
+	
+	ft_printf("%X\n", -16);
+	printf("%X\n", -16);
+	
+	ft_printf("====\ncheck %%\n====\n");
+	ft_printf("%%\n");
+	printf("%%\n");
+	
+	ft_printf("====\ncheck combination\n====\n");
+	ft_printf("%d %s %u %x%%\n",123,"abc",1111,123);
+	printf("%d %s %u %x%%\n",123,"abc",1111,123);
+	ft_printf("%X %x %u %p%%\n",123,0,1111,"123");
+	printf("%X %x %u %p%%\n",123,0,1111,"123");
+	ft_printf("%d %d %d %d %d %d %d\n",123,1111,123,1111,0,1255,66);
+	printf("%d %d %d %d %d %d %d\n",123,1111,123,1111,0,1255,66);
+	ft_printf("%c %c %c %c %c %c %c\n",123,1111,123,1111,0,1255,66);
+	printf("%c %c %c %c %c %c %c\n",123,1111,123,1111,0,1255,66);
 }
