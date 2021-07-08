5040a5aac4fd676d7f3e551b0073cb469261bf80
Hyunwoo Kim <hyunwkim@c10r6s6.42seoul.kr>
2021-07-08 18:31:51 +0900

    feat: done

--- ft_printf.h
+++ ft_printf.h
@@ -6,20 +6,19 @@
 /*   By: hyunwkim <hyunwkim@42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 14:00:07 by hyunwkim          #+#    #+#             */
-/*   Updated: 2021/07/08 18:14:58 by hyunwkim         ###   ########.fr       */
+/*   Updated: 2021/07/08 18:29:55 by hyunwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
-#define ERR -1
-#define TYPE "cspdiuxX%"
-#define FLAG "-0.*"
+# define FT_PRINTF_H
+# define ERR -1
+# define TYPE "cspdiuxX"
 
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "libft/libft.h"
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
 
 int		ft_printf(const char *line, ...);
 int		check_format(const char *line, va_list ap, int *rtn);
@@ -29,5 +28,6 @@ int		print_nbr(long long n, char type, int *rtn);
 int		get_hex_digits(unsigned long long n);
 char	*get_base(char c);
 void	ft_putchar(char c, int *rtn);
-void	ft_putstr(char *s, int *rtn); 
+void	ft_putstr(char *s, int *rtn);
+
 #endif
