c8b10550066d256b64b3be6d6f4fba78f6da6360
ab8454 <ab8454@naver.com>
2021-07-14 17:43:56 +0900

    fix -. flag

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hyunwkim <hyunwkim@42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 13:53:14 by hyunwkim          #+#    #+#             */
-/*   Updated: 2021/07/12 12:34:44 by hyunwkim         ###   ########.fr       */
+/*   Updated: 2021/07/14 17:43:33 by hyunwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ void	get_asterisk(t_info *info, va_list *ap)
 }
 
 int	get_format_info(const char *line, t_info *info, va_list *ap)
+//int	get_format_info(const char *line, t_info *info)
 {
 	int	idx;
 
@@ -62,11 +63,14 @@ int	get_format_info(const char *line, t_info *info, va_list *ap)
 		idx += is_num(&line[idx], info);
 	while (ft_strchr(FLAG, line[idx]))
 	{
+		if (info->dot == 1 && line[idx] == '0')
+			break ;
 		get_flags(line[idx], info);
+		
 		if ('*' == line[idx++])
 			get_asterisk(info, ap);
 	}
-	if ('1' <= line[idx] && line[idx] <= '9' && !info->asterisk)
+	if ('0' <= line[idx] && line[idx] <= '9' && !info->asterisk)
 		idx += is_num(line + idx, info);
 	if (ft_strchr(TYPE, line[idx]) || line[idx] == '%')
 		info->type = line[idx++];
@@ -81,6 +85,7 @@ int	check_format(const char *line, t_info *info, va_list *ap)
 
 	init_info(info);
 	rtn = get_format_info(line, info, ap);
+	//rtn = get_format_info(line, info);
 	if (info->type == '%')
 		return (print_char('%', info) + rtn);
 	if (rtn == ERR)
@@ -91,9 +96,9 @@ int	check_format(const char *line, t_info *info, va_list *ap)
 		return (print_str(va_arg(*ap, char *), info) + rtn);
 	//if (info->type == 'p' || info->type == 'x' || info->type == 'X')
 	if (info->type == 'p')
-		return (print_hex(va_arg(*ap, unsigned long long), info) + rtn);
+		return (parse_hex(va_arg(*ap, unsigned long long), info) + rtn);
 	if (info->type == 'x' || info->type == 'X')
-		return (print_hex(va_arg(*ap, unsigned int), info) + rtn);
+		return (parse_hex(va_arg(*ap, unsigned int), info) + rtn);
 	if (info->type == 'd' || info->type == 'i')
 		return (print_num(va_arg(*ap, int), info) + rtn);
 	if (info->type == 'u')
@@ -282,14 +287,16 @@ int	print_num(long long n, t_info *info)
 			if (get_int_digits(n) > info->width)
 				info->zero = 1;
 			print_multi_str(info->prec - get_int_digits(n), info);
-			ft_putnbr(n, info);
+			if (n)
+				ft_putnbr(n, info);
 		}
 		else 
 			ft_putnbr(n, info);
 	}
 	else 
 	{
-		if (info->prec > get_int_digits(n))
+		//if (info->prec > get_int_digits(n))
+		if (info->prec >= get_int_digits(n))
 		{
 			if (info->width > info->prec)
 			{
@@ -323,7 +330,7 @@ int	print_num(long long n, t_info *info)
 			else
 				print_multi_str(info->width - get_int_digits(n), info);
 		}
-		if (info->prec != 0 || info->dot == -1)
+		if (n || info->prec > 0 || info->dot != 1)
 			ft_putnbr(n, info);
 	}
 	return (1);
@@ -358,18 +365,24 @@ char	*get_base(char c)
 	return (0);
 }
 
-int	print_hex(unsigned long long n, t_info *info)
+int	parse_hex(unsigned long long n, t_info *info)
 {
 	char	*hex_arr;
 	int		hex_len;
 
 	hex_len = get_hex_len(n);
+	if (info->type == 'p')
+		hex_len += 2;
 	hex_arr = (char *)malloc(sizeof(char) * (hex_len + 1));
 	if (!hex_arr)
 		return (ERR);
 	hex_arr[hex_len] = 0;
 	if (info->type == 'p')
-		ft_putstr("0x", 2, info);
+	{
+		hex_arr[0] = '0';
+		hex_arr[1] = 'x';
+		hex_arr[2] = '0';
+	}
 	if (n)
 	{
 		while (n)
@@ -378,13 +391,83 @@ int	print_hex(unsigned long long n, t_info *info)
 			n /= 16;
 		}
 	}
-	else 
+	else if (info->type != 'p' || n)
 		hex_arr[0] = '0';
-	//print_str(hex_arr, info);
-	print_num(ft_atoi(hex_arr), info);
+	print_hex(hex_arr, info);
 	free(hex_arr);
 	return (1);
 }
+int	print_hex(char *s, t_info *info)
+{
+	if (info->left_align)
+	{
+		if (info->width > info->prec && info->prec > (int)ft_strlen(s))
+		{
+			info->zero = 1;
+			print_multi_str(info->prec - (int)ft_strlen(s), info);
+			ft_putstr(s, (int)ft_strlen(s), info);
+			info->zero = 0;
+			print_multi_str(info->width - info->prec, info);
+		}
+		else if (info->width > (int)ft_strlen(s) && (int)ft_strlen(s) > info->prec)
+		{
+			if (s[0] - '0' == 0 && info->dot == 1)
+				print_multi_str(info->width, info);
+			else
+			{
+				ft_putstr(s, (int)ft_strlen(s), info);
+				print_multi_str(info->width - (int)ft_strlen(s), info);
+			}
+		}
+		else if (info->prec >= info->width ||  info->prec > (int)ft_strlen(s))
+		{
+			if ((int)ft_strlen(s) > info->width)
+				info->zero = 1;
+			print_multi_str(info->prec - (int)ft_strlen(s), info);
+			if (s[0] - '0' != 0)
+				ft_putstr(s, (int)ft_strlen(s), info);
+			else if (s[1] == 'x')
+				ft_putstr("0x", 2, info);
+		}
+		else 
+			ft_putstr(s, (int)ft_strlen(s), info);
+	}
+	else 
+	{
+		if (info->prec > (int)ft_strlen(s))
+		{
+			if (info->width > info->prec)
+			{
+				info->zero = 0;
+				print_multi_str(info->width - info->prec, info);
+			}
+		
+			info->zero = 1;
+			print_multi_str(info->prec - (int)ft_strlen(s), info);
+		}
+		else if (info->prec >= info->width && info->width > (int)ft_strlen(s))
+			print_multi_str(info->prec - (int)ft_strlen(s), info);
+		else if (info->width > (int)ft_strlen(s) && (int)ft_strlen(s) > info->prec)
+		{
+			if (info->dot == -1 && info->zero)
+				info->zero = 1;
+			else
+				info->zero = 0;
+			if (s[0] - '0' == 0 && info->dot == 1 && info->type == 'p')
+				print_multi_str(info->width - 2, info);
+			else if (s[0] - '0' == 0 && info->dot == 1)
+				print_multi_str(info->width, info);
+			else
+				print_multi_str(info->width - (int)ft_strlen(s), info);
+		}
+		if (info->prec > 0 || info->dot != 1 || (info->type == 'p' && ft_strncmp(s,"0x0",3)))
+			ft_putstr(s, (int)ft_strlen(s), info);
+		else if (info->type == 'p')
+			ft_putstr("0x", 2, info);
+	}
+	return (1);
+}
+
 int	get_hex_len(unsigned long long n)
 {
 	int	i;
@@ -399,6 +482,7 @@ int	get_hex_len(unsigned long long n)
 	}
 	return (i);
 }
+
 void	ft_putnbr(long long n, t_info *info)
 {
 	if (n < 0)
@@ -417,6 +501,16 @@ void	ft_putnbr(long long n, t_info *info)
 //#include<stdio.h>
 //int main()
 //{
-//	ft_printf("%.5x\n", 21);
-//	printf("%.5x\n", 21);
+//	ft_printf("%-.p\n",NULL);
+//	printf("%-.p\n",NULL);
+//	ft_printf("%-.s\n","");
+//	printf("%-.s\n","");
+//	ft_printf("%-.d\n",0);
+//	printf("%-.d\n",0);
+//	ft_printf("%-.u\n",0);
+//	printf("%-.u\n",0);
+//	ft_printf("%-.x\n",0);
+//	printf("%-.x\n",0);
+//	ft_printf("%-.X\n",0);
+//	printf("%-.X\n",0);
 //}
